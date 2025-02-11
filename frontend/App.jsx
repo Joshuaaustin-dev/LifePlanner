ecb6679f28ec2081a7235f2acd61a8e19905061a
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-02-10 19:00:07 -0700

    redsign of the navigation menu

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
 import NavigationMenu from "./components/NavigationMenu";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Planner from "./components/Planner/Planner";
 import Profile from "./components/Profile/Profile";
 import Calendar from "./components/Calender/Calender";
+
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-      <Router>
+    <Router>
+      <div className="App">
         <NavigationMenu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Planner" element={<Planner />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Calender" element={<Calendar />} />
-        </Routes>
-      </Router>
-    </div>
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/Planner" element={<Planner />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Calender" element={<Calendar />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- frontend/components/NavigationMenu.jsx
+++ frontend/components/NavigationMenu.jsx
@@ -1,17 +1,24 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import styles from "./NavigationMenu.module.css";
+import logo from "../assets/images/LifePlanner.png";
 
 const NavigationMenu = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand href="/"></Navbar.Brand>
+    <Navbar className={`${styles.sidebar} NavigationMenu`}>
+      <Container className="d-flex flex-column align-items-center">
+        <div className={styles.logoContainer}>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </Navbar.Brand>
+        </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+          <Nav className="flex-column">
+            <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
             <Nav.Link href="/Planner">Planner</Nav.Link>
             <Nav.Link href="/Profile">Profile</Nav.Link>
             <Nav.Link href="/Calender">Calendar</Nav.Link>
+            <Nav.Link href="/Calender">Shop</Nav.Link>
             <Nav.Link href="/SignOut">Sign Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
