c3929c86113f992691f4a553886b4f9502cb1dec
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-02-20 18:58:24 -0700

    updated nav styling

--- frontend/components/NavigationMenu.jsx
+++ frontend/components/NavigationMenu.jsx
@@ -4,22 +4,35 @@ import logo from "../assets/images/LifePlanner.png";
 
 const NavigationMenu = () => {
   return (
-    <Navbar className={`${styles.sidebar} NavigationMenu`}>
-      <Container className="d-flex flex-column align-items-center">
+    <Navbar className={`${styles.sidebar}`} expand="lg">
+      <Container className="d-flex flex-column">
         <div className={styles.logoContainer}>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="/" className={styles.brandContainer}>
+            <h3 className={styles.brandTitle}>LifePlanner</h3>
             <img src={logo} alt="Logo" className={styles.logo} />
           </Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="flex-column">
-            <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
-            <Nav.Link href="/Planner">Planner</Nav.Link>
-            <Nav.Link href="/Profile">Profile</Nav.Link>
-            <Nav.Link href="/Calender">Calendar</Nav.Link>
-            <Nav.Link href="/Calender">Shop</Nav.Link>
-            <Nav.Link href="/Logout">Sign Out</Nav.Link>
+          <Nav className={`flex-column ${styles.navLinks}`}>
+            <Nav.Link href="/Dashboard" className={styles.navItem}>
+              Dashboard
+            </Nav.Link>
+            <Nav.Link href="/Planner" className={styles.navItem}>
+              Planner
+            </Nav.Link>
+            <Nav.Link href="/Profile" className={styles.navItem}>
+              Profile
+            </Nav.Link>
+            <Nav.Link href="/Calender" className={styles.navItem}>
+              Calendar
+            </Nav.Link>
+            <Nav.Link href="/Shop" className={styles.navItem}>
+              Shop
+            </Nav.Link>
+            <Nav.Link href="/Logout" className={styles.navItem}>
+              Sign Out
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
