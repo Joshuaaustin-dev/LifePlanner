00d6669d7199fef44edb0a90248bced7c19b746b
Joshuaaustin-dev <austin.j0sh135@gmail.com>
2025-03-13 13:06:15 -0600

    Updated nav design and made responsive

--- frontend/components/NavigationMenu.jsx
+++ frontend/components/NavigationMenu.jsx
@@ -1,40 +1,67 @@
+import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import styles from "./NavigationMenu.module.css";
 import logo from "../assets/images/LifePlanner.png";
+import { TfiDashboard } from "react-icons/tfi";
+import { IoMdBook } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { FaShop } from "react-icons/fa6";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const NavigationMenu = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Update the state based on screen width
+  const handleResize = () => {
+    if (window.innerWidth < 800) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // Listen to window resize event
+  useEffect(() => {
+    handleResize(); // Initial check on component mount
+    window.addEventListener("resize", handleResize); // Add resize listener
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Navbar className={`${styles.sidebar}`} expand="lg">
-      <Container className="d-flex flex-column">
-        <div className={styles.logoContainer}>
-          <Navbar.Brand href="/" className={styles.brandContainer}>
-            <h3 className={styles.brandTitle}>LifePlanner</h3>
+      <Container className={`d-flex ${isMobile ? "flex-row" : "flex-column"}`}>
+        <Nav
+          className={`${isMobile ? "flex-row" : "flex-column"} ${
+            styles.navLinks
+          }`}
+        >
+          <Nav.Link href="/" className={styles.navLogo}>
             <img src={logo} alt="Logo" className={styles.logo} />
-          </Navbar.Brand>
-        </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={`flex-column ${styles.navLinks}`}>
-            <Nav.Link href="/Dashboard" className={styles.navItem}>
-              Dashboard
-            </Nav.Link>
-            <Nav.Link href="/Planner" className={styles.navItem}>
-              Planner
-            </Nav.Link>
-            <Nav.Link href="/Profile" className={styles.navItem}>
-              Profile
-            </Nav.Link>
-            <Nav.Link href="/Calender" className={styles.navItem}>
-              Calendar
-            </Nav.Link>
-            <Nav.Link href="/Shop" className={styles.navItem}>
-              Shop
-            </Nav.Link>
-            <Nav.Link href="/Logout" className={styles.navItem}>
-              Sign Out
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          </Nav.Link>
+          <Nav.Link href="/Dashboard" className={styles.navItem}>
+            <TfiDashboard /> <span>Dashboard</span>
+          </Nav.Link>
+          <Nav.Link href="/Planner" className={styles.navItem}>
+            <IoMdBook /> <span>Planner</span>
+          </Nav.Link>
+          <Nav.Link href="/Profile" className={styles.navItem}>
+            <CgProfile /> <span>Profile</span>
+          </Nav.Link>
+          <Nav.Link href="/Calender" className={styles.navItem}>
+            <FaRegCalendarAlt /> <span>Calendar</span>
+          </Nav.Link>
+          <Nav.Link href="/Shop" className={styles.navItem}>
+            <FaShop /> <span>Shop</span>
+          </Nav.Link>
+          <Nav.Link href="/Logout" className={styles.navItem}>
+            <FaSignOutAlt /> <span>Log out</span>
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
